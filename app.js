836759d39cb7d000b3e91f4970d75d8f0198eafe
spardutti <luisdamian.sp@gmail.com>
2020-09-03 21:43:12 -0300

    we did it! it works! missing decimals and delete

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const display = document.querySelector(".display");
 const button = document.querySelectorAll(".numBtn");
 const operators = document.querySelectorAll(".opBtn");
+const equal = document.getElementById("equal");
+const clearAll = document.querySelector(".btn-clear");
+const del = document.querySelector(".btn-del");
 
 let num1 = "";
 let num2 = "";
@@ -18,43 +21,111 @@ button.forEach((btn) => {
 operators.forEach((op) => {
   op.addEventListener("click", displayOperator);
 });
+equal.addEventListener("click", equalCal);
 
+clearAll.addEventListener("click", clear);
+
+del.addEventListener("click", deleteN)
+
+//calculate operation when hitting =
+function equalCal() {
+  if (!num2) {
+    display.value = num1;
+  } else {
+    display.value = calculate(num1, operator, num2);
+  }
+}
+//clear all function
+function clear() {
+  num1 = "";
+  num2 = "";
+  operatorActive = false;
+  operator = "";
+  calculationDone = false;
+  display.value = "";
+}
+//IN PRGORESS
+function deleteN() {
+  if (num1) {
+    let strn = num1.toString()
+    str
+  }
+}
+//display numbers
 function displayNum(num) {
   num = num.target.value;
-  if (!operatorActive && !calculationDone) {
+  //if no operator selected, display will be num1
+  if (!operatorActive) {
     display.value += num;
     num1 = parseInt(display.value);
   }
-  if (operatorActive) {
+  //if operator is selected but calculations are not done, display will be num2
+  if (operatorActive && !calculationDone) {
     display.value += num;
     num2 = parseInt(display.value);
   }
- 
-  console.log(num1, num2, operator);
+  //if oper and calcul are done, display will be num1, and we clear display
+  //then num2 will be display
+  if (calculationDone) {
+    num1 = parseInt(display.value);
+    num2 = "";
+    calculationDone = false;
+    if (num2 == "") {
+      display.value = "";
+    }
+    display.value += num;
+    num2 = parseInt(display.value);
+    console.log("w");
+  }
+  console.log(num1, operator, num2);
 }
-
+//listen to operators
 function displayOperator(oper) {
-  oper = oper.target.value;
-  operatorActive = true;
-  display.value = "";
-  if (num1 && num2) {
-    display.value = calculate(num1, oper, num2)
+  //if no operator, set it to true, get the operator and reset display.
+  if (!operatorActive) {
+    operator = oper.target.value;
     operatorActive = true;
-    calculationDone = true;
+    display.value = "";
   }
-  if (oper === "=") {
-    display.value = total;
+  //if we have both num and operator, calculate
+  if (num1 && num2 && !calculationDone) {
+    calculate(num1, operator, num2);
   }
-  
+  //if calc is done, change the operator when button is cliced
+  if (calculationDone) {
+    operator = oper.target.value;
+  }
+  console.log(operator, operatorActive);
 }
-
+//do the magic math
 function calculate(value1, oper, value2) {
   switch (oper) {
     case "+":
       total = value1 + value2;
+      calculationDone = true;
+      display.value = total;
+      break;
+    case "X":
+      total = value1 * value2;
+      calculationDone = true;
+      display.value = total;
+
+      break;
+    case "-":
+      total = value1 - value2;
+      calculationDone = true;
+      display.value = total;
+
+      break;
+    case "/":
+      total = value1 / value2;
+      calculationDone = true;
+      display.value = total;
+
       break;
     default:
-      return false;
+      console.log("here");
+      return;
   }
   return total;
 }
